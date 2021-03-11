89a3372dd1c27fb0bd772594d7d5b1621ce0cb47
KobyBrown <kobyah@hawaii.edu>
2021-03-11 11:46:43 -1000

    DOTW1

---
+++ WODs/DayOfTheWeek1/DOTW1.js
@@ -0,0 +1,29 @@
+day = 30;
+month = 'Aug';
+year = 1999;
+
+if (month=="Jan" | month == "Feb"){
+    step1 = year -2;
+} else {
+    step1 = year -1
+}
+step2 = parseInt(step1/4) + step1;
+step3 = step2 - parseInt(step1/100);
+step4 = parseInt(step1/400);
+step5 = day + step4;
+var monthKey = {
+    'Jan' :	0,
+'Feb' :3,
+'Mar' :2,
+'Apr':5,
+'May':0,
+'Jun':3,
+'Jul':5,
+'Aug':1,
+'Sep':4,
+'Oct':6,
+'Nov':	2,
+'Dec':	4,
+}
+step6 = monthKey[month] + step5;
+step7 = step6 % 7;
