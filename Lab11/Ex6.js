ed88d9b1d1bfaf26cfe759767e6eb6cc8e3b3024
KobyBrown <kobyah@hawaii.edu>
2021-05-09 23:14:44 -1000

    Update 5/9/21

---
+++ Lab11/Ex6.js
@@ -0,0 +1,18 @@
+function checkIt(item, index){
+    console.log(`part ${index} is ${(isNonNegInt(item)?'a':'not a')} quantity`);
+}
+
+function isNonNegInt(q) {
+    errors = []; // assume no errors at first
+if(Number(q) != q) errors.push('Not a number!'); // Check if string is a number value
+if(q < 0) errors.push('Negative value!'); // Check if it is non-negative
+if(parseInt(q) != q) errors.push('Not an integer!'); // Check that it is an integer
+
+return returnErrors ? errors : (errors.length == 0);
+}
+
+pieces = 'Koby;21;21.5;-21.5;'.split(";");
+
+// pieces.forEach(checkIt);
+
+pieces.forEeach((item,index) => {} )
---
+++ Lab7/SmartPhoneProducts1_4/junk.js
@@ -0,0 +1,7 @@
+age =  21;
+
+fav_num = 7;
+
+day_of_birth = 30;
+
+month_of_birth = 8;
