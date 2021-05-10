c863c51b493b38989dee555b0d34b5b0d1537e35
KobyBrown <kobyah@hawaii.edu>
2021-05-10 11:49:34 -1000

    5/10/21 Update

--- WODs/Invoice4/products_data.js
+++ WODs/Invoice4/products_data.js
@@ -1,25 +1,25 @@
 var products = [
-    {name : "HTC",
+    {"brand" : "HTC",
     "price" : 40.00,
     "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/HTC.jpg"},
 
     
-     {name : "Apple",
+     {"brand" : "Apple",
      "price" : 75.00,
      "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/iphone-3gs.jpg"},
 
      
-     {name : "Nokia",
+     {"brand" : "Nokia",
      "price" : 35.00,
      "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Nokia.jpg"},
 
      
-     {name : "Samsung",
+     {"brand" : "Samsung",
      "price" : 45.00,
-     image : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"},
+     "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Samsung.jpg"},
 
      
-     {name : "Blackberry",
+     {"brand" : "Blackberry",
      "price" : 10.00,
      "image" : "http://dport96.github.io/ITM352/morea/080.flow-control-II/Blackberry.jpg"},
 ];
